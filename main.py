4ecbe895dd276631809a00b1be306b55a011995a
tranngoc769 <tranngoc769@gmail.com>
2021-04-27 07:35:31 +0700

    config, log, update source

--- main.py
+++ main.py
@@ -1,32 +1,91 @@
-#import thư viện càn dùng
-# import undetected_chromedriver as uc
-#thư viện python dùng để lấy dữ liệu ra khỏi HTML và XML
 from bs4 import BeautifulSoup
 import re
-#thư viện requests
 import requests
 import json
 import os
 import sys
 import csv
+import logging
+from configparser import ConfigParser
+# Global Variable
+config_object = ConfigParser()
 checkpoint_dir = "checkpoint"
-temp_checkpoint = ""
+excel_name = "data.csv"
+txt_name = "data.txt"
+log_path = "erro.log"
+mode = 1
+logger = None
+# 
+# Get all config variable
 def init():
+    global checkpoint_dir,txt_name,txt_name,log_path,mode,excel_name, logger
+    config = None
+    try:
+        config_object.read("config.ini")
+        config = config_object["APPLICATION_CONFIG"]
+    except Exception as err:
+        print("Cannot read config.ini")
+    try:
+        log_path = config["log_file"]
+    except Exception as err:
+        print("Cannot init log path: ",str(err))
+    try:
+        logging.basicConfig(filename=log_path, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        logger = logging.getLogger(__name__)
+        # Create handlers
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler(log_path)
+        c_handler.setLevel(logging.WARNING)
+        f_handler.setLevel(logging.ERROR)
+        # Create formatters and add it to handlers
+        c_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        c_handler.setFormatter(c_format)
+        f_handler.setFormatter(f_format)
+        # Add handlers to the logger
+        if (logger.hasHandlers()):
+            logger.handlers.clear()
+        logger.addHandler(c_handler)
+        logger.addHandler(f_handler)
+        logger.propagate = False
+    except Exception as err:
+        print("Cannot init logging: ",str(err))
+    if config == None:
+        return
+    try:
+        excel_name = config["excel_name"]
+    except Exception as err:
+        logger.error("Cannot read excel_name, using default: %s", excel_name)
+    try:
+        txt_name = config["txt_name"]
+    except Exception as err:
+        logger.error("Cannot read txt_name, using default: %s", txt_name)
+    try:
+        checkpoint_dir = config["checkpoint_dir"]
+    except Exception as err:
+        logger.error("Cannot read checkpoint_dir, using default: %s", checkpoint_dir)
+    try:
+        mode = config["mode"]
+    except Exception as err:
+        logger.error("Cannot read mode, using default: %s", mode)
     if os.path.isdir(checkpoint_dir)==False:
         os.mkdir(checkpoint_dir)
 def appendFieldCsv(field):
-    with open(r'sample.csv', 'a', newline='\n') as f:
+    global excel_name
+    with open(excel_name, 'a', newline='\n') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
-def getAllLinkFromFile(filepath):
-    with open(filepath, 'r') as f:
+def getAllLinkFromFile():
+    global txt_name
+    with open(txt_name, 'r') as f:
         lines = f.read().splitlines() 
         return lines
     return []
 def GetFieldInLink(URL):
+    global logger
     try:
         num_images = ''
-        print("Crawl : "+URL)
+        logger.warning("Crawl: %s",URL)
         x = requests.get(url = URL)
         soup = BeautifulSoup(x.text,'html.parser')
         images = soup.find('a',{'class':"open-list-full-list-link"})
@@ -40,25 +99,29 @@ def GetFieldInLink(URL):
         link_point = votes.find('div',{'class':"points"}).text.strip()
         list_image = soup.find('div',{'class':"open-list-item open-list-block clearfix"})
         first_img_point = list_image.find('div',{'class':"points"}).text.strip().replace('points','')
-        print("Link Point : "+ link_point + "--" + "Images : "+ num_images + "--" +"FIP : "+ first_img_point)
+        logger.warning("Point: "+ link_point + "," + "Images: "+ num_images + "," +"FIP: "+ first_img_point)
         return [URL, link_point,num_images,first_img_point]
     except Exception as err:
-        print("Failed : "+URL+" -- Erro : "+str(err))
+        logger.warning("Failed: "+URL+",Erro: "+str(err))
         return False
 def removeCheckpoint(checkpoint_file):
+    global checkpoint_dir
     try:
         os.remove(checkpoint_dir+"/"+ checkpoint_file)
     except:
         pass
 def makeCheckpoint(checkpoint_file):
+    global checkpoint_dir
     try:
         with open(checkpoint_dir+"/"+ checkpoint_file,"w") as f:
             pass
     except:
         pass
 def checkCheckpoint(checkpoint_file):
+    global checkpoint_dir
     return os.path.isfile(checkpoint_dir+"/"+ checkpoint_file)
 def getAllLinkFromIndexPage(page):
+    global logger
     links = []
     try:
         x = requests.get("https://www.boredpanda.com/page/"+str(page))
@@ -74,12 +137,11 @@ def getAllLinkFromIndexPage(page):
             links.append("https://www.boredpanda.com/"+url)
         return links
     except Exception as er:
-        print(str(er))
+        logger.error(str(er))
         return links
 if __name__=='__main__':
     init()
-    mode = 1
-    if mode == 1:
+    if mode == 1  or mode == '1':
         page = 0
         run = True
         while run:
@@ -94,10 +156,10 @@ if __name__=='__main__':
                         appendFieldCsv(fields)
                         makeCheckpoint(sub_name)
                 else:
-                    print("Checkpoint! End program")
+                    logger.warning("React to checkpoint! End program")
                     exit()
             page += 1
-    links = getAllLinkFromFile("data.txt")
+    links = getAllLinkFromFile()
     for link in links:
         sub_name = link.replace('https://www.boredpanda.com/','')
         sub_name = sub_name.replace('-','_')
@@ -108,4 +170,5 @@ if __name__=='__main__':
                 appendFieldCsv(fields)
                 makeCheckpoint(sub_name)
         else:
-            print("Existed")
+            logger.warning("React to checkpoint! End program")
+            exit()
